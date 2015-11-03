f43f45887175ad5c5e84aca4493d446874a43fdf
Philip Hofmann <philip.hofmann@uzh.ch>
2015-11-03 08:48:29 +0100

    My upload
    Changed server request so it gets all the data (columns header). Changed
    Header Columns it shows at the beginning. And some names. Commented the
    code out so I know what the code actually does (can keep it or discard
    it)

--- GreetingServiceImpl.java
+++ GreetingServiceImpl.java
@@ -16,110 +16,87 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
  */
 @SuppressWarnings("serial")
 public class GreetingServiceImpl extends RemoteServiceServlet implements GreetingService {
-	
-    private ArrayList <String> executeQuery(String query) {
-    ArrayList <String> queryResult = new ArrayList <String> ();
 
-    Connection connection = null;
-    ResultSet resultSet = null;
-    ResultSetMetaData rsmd = null;
-    Statement statement = null;
-				
-        try
-        {
-            Class.forName("com.mysql.jdbc.Driver");
-            //Class.forName("com.mysql.jdbc.GoogleDriver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            // TODO Auto-generated catch block
-        }
-				
-        try
-        {
-            //connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test2?user=root");
-            connection = DriverManager.getConnection("jdbc:mysql://62.203.130.55:8088/moviemaptest", "SEUser", "SE2015DB");
-        }
-        catch (SQLException e)
-        {
-            // TODO Auto-generated catch block
-        }
-    
-        if (connection != null)
-        {
-            try
-            {
-                statement = connection.createStatement();
-            }
-            catch (SQLException e)
-            {
-                // TODO Auto-generated catch block
-            }
-        }
-        
-        if (statement != null)
-        {
-            try
-            {
-                resultSet = statement.executeQuery(query);
-            }
-            catch (SQLException e)
-            {
-                // TODO Auto-generated catch block
-            }
-        }
-        
-        try
-        {
-            rsmd = resultSet.getMetaData();
-        }
-        catch (SQLException e)
-        {
-            // TODO Auto-generated catch block
-        }
-        
-        try
-        {
-            queryResult.add(rsmd.getColumnCount() + "");
-        }
-        catch (SQLException e)
-        {
-            // TODO Auto-generated catch block
-        }
-        
-        try
-        {
-            for (int i = 1; i <= rsmd.getColumnCount(); i++)
-            {
-                queryResult.add(rsmd.getColumnName(i));
-            }
-        }
-        catch (SQLException e)
-        {
-            // TODO Auto-generated catch block
-        }
-        
-        try
-        {
-            while (resultSet.next())
-            {
-                for (int i=1; i<=rsmd.getColumnCount(); i++)
-                {
-                    queryResult.add(resultSet.getString(i));
-                }
-            }
-        }
-        catch (SQLException e)
-        {
-            // TODO Auto-generated catch block
-        }
-        
-        return queryResult;
-    }
-	
-	public ArrayList<String> getTableData() {
-        String tableName = "movies1";
-		return executeQuery("SELECT name, length, language, origin FROM " + tableName + " WHERE year=2000");
+	private ArrayList<String> executeQuery(String query) {
+		ArrayList<String> queryResult = new ArrayList<String>();
+
+		Connection connection = null;
+		ResultSet resultSet = null;
+		ResultSetMetaData rsmd = null;
+		Statement statement = null;
+
+		try {
+			// this loads the MySQL driver
+			Class.forName("com.mysql.jdbc.Driver");
+			// Class.forName("com.mysql.jdbc.GoogleDriver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+		}
+
+		try {
+			// sets up connection with the DB
+			// connection =
+			// DriverManager.getConnection("jdbc:mysql://localhost:3306/test2?user=root");
+			connection = DriverManager.getConnection("jdbc:mysql://62.203.130.55:8088/moviemaptest", "SEUser",
+					"SE2015DB");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+		}
+
+		// statements allow to issue SQL queries to the DB
+		if (connection != null) {
+			try {
+				statement = connection.createStatement();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+			}
+		}
+
+		// get the result of the sql query
+		if (statement != null) {
+			try {
+				resultSet = statement.executeQuery(query);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+			}
+		}
+
+		try {
+			rsmd = resultSet.getMetaData();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+		}
+
+		try {
+			queryResult.add(rsmd.getColumnCount() + "");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+		}
+
+		try {
+			for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+				queryResult.add(rsmd.getColumnName(i));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+		}
+
+		try {
+			while (resultSet.next()) {
+				for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+					queryResult.add(resultSet.getString(i));
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+		}
+		return queryResult;
 	}
 
+	@Override
+	public ArrayList<String> getTableData() {
+		String tableName = "movies1";
+		return executeQuery(
+				"SELECT ID, Name, Year, Length, Language, Origin, Genre FROM " + tableName + " WHERE year=2000");
+	}
 }
--- MovieMap.java
+++ MovieMap.java
@@ -1,11 +1,7 @@
 package com.client;
-//test
-
-
-import com.client.GreetingService;
-import com.client.GreetingServiceAsync;
 
 import java.util.ArrayList;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -26,76 +22,87 @@ public class MovieMap implements EntryPoint {
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
 	 */
-private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
-	
-	private VerticalPanel mainPanel = new VerticalPanel();
-	private FlexTable dataTable = new FlexTable();
-	private HorizontalPanel addPanel = new HorizontalPanel();
-	private Button query = new Button("executeQuery");
+	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 
+	// Constructor. Creates new Instance of Flextable
+	// Flextable automatically resizes on demand - there is no explicit size
+	private final FlexTable dataTable = new FlexTable();
+	// Constructors. Creates new Instances of Panels
+	// HorizontalPanel grows to the right if items are added
+	// VerticalPanel grows downwards if items are added
+	private final HorizontalPanel addPanel = new HorizontalPanel();
+	private final VerticalPanel mainPanel = new VerticalPanel();
+
+	// Constructor. Creates new Instance of Button
+	private final Button button = new Button("GO");
 
 	/**
 	 * This is the entry point method.
 	 */
+	@Override
 	public void onModuleLoad() {
-		
-		//Create datatable
-		dataTable.setText(0,0,"Name");
-		dataTable.setText(0,1,"Length");
-		dataTable.setText(0,2,"Language");
-		dataTable.setText(0,3,"Origin");
-				
-		// Add styles to elements datatable.
+
+		// Sets Table Headers (Columns)
+		// Method "setText" is inherited from HTMLTable class
+		dataTable.setText(0, 0, "ID");
+		dataTable.setText(0, 1, "Name");
+		dataTable.setText(0, 2, "Year");
+		dataTable.setText(0, 3, "Length");
+		dataTable.setText(0, 4, "Language");
+		dataTable.setText(0, 5, "Origin");
+		dataTable.setText(0, 6, "Genre");
+
+		// Add styles to elements of dataTable
 		dataTable.setCellPadding(6);
-		
-        //Add button to horizontalpanel
-		addPanel.add(query);
-		
-        //Add buttonpanel and datatable to mainPanel
+
+		// Adds button to addPanel from HorizontalPanel
+		addPanel.add(button);
+
+		// Adds addPanel from HorizontalPanel (=button) and dataTable to
+		// mainPanel from VerticalPanel
+		// Meaning: mainPanel contains the Table, and underneath it the button
+		// (wrapped in addPanel)
 		mainPanel.add(dataTable);
 		mainPanel.add(addPanel);
-		
-        //Add mainPanel to RootPanel with id=root
-		RootPanel.get("root123").add(mainPanel);
-		
-        //Handle click on button
-		query.addClickHandler(new ClickHandler()
-		{
-			public void onClick(ClickEvent event)
-			{
+
+		// Add mainPanel to RootPanel with id=mainPanel -> we use it in the
+		// .html for the div element for more control over our layout.
+		RootPanel.get("mainPanel").add(mainPanel);
+
+		// Handle click on button
+		button.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
 				clicked();
 			}
 		});
 	}
-	
+
 	private void clicked() {
-		greetingService.getTableData(new AsyncCallback<ArrayList <String>>()
-		{
-				public void onFailure(Throwable caught)	
-				{
-					Window.alert(caught.getMessage());
-				}
-				public void onSuccess(ArrayList <String> result)
-				{
-					fillTable(result);
-				}
-								
+		// AsyncCallback(returnType) - Interface to receive response from RPC
+		greetingService.getTableData(new AsyncCallback<ArrayList<String>>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert(caught.getMessage());
+			}
+
+			@Override
+			public void onSuccess(ArrayList<String> result) {
+				fillTable(result);
+			}
 		});
-		
 	}
 
-		
 	private void fillTable(ArrayList<String> data) {
-		
-		if(!(data != null)) return;
-		int columnsCount= Integer.parseInt(data.get(0));
-		int cnt=1;
-		for(int i=0;i< ((data.size()-1)/columnsCount);i++){
-				for(int c=0;c<columnsCount;c++){
-					dataTable.setText(i,c,data.get(cnt));
-					cnt++;
-                }
+		if (!(data != null))
+			return;
+		final int columnsCount = Integer.parseInt(data.get(0));
+		int entry = 1;
+		for (int i = 0; i < ((data.size() - 1) / columnsCount); i++) {
+			for (int c = 0; c < columnsCount; c++) {
+				dataTable.setText(i, c, data.get(entry));
+				entry++;
+			}
 		}
 	}
-    
 }
